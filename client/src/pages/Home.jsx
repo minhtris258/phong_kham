329537c7912cde412fcd3fab9c7adcbabe73eea2
TriNguyen <tringuyen.2372004@gmail.com>
2025-12-10 19:01:10 +0700

    update servicehome page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,33 +1,32 @@
 // src/pages/Home.jsx (hoặc đâu mà bạn đang để)
-import React from 'react';
-import Hero from '../components/Hero';
-import PartnersSection from '../components/PartnersSection';
-import DoctorsFavorite from '../components/DoctorsFavorite';
-import SpecialtySection from '../components/SpecialtySection';
-import DownloadAppSection from '../components/DownloadAppSection';
-import HomePosts from '../components/post/HomePosts';
-import StatsSection from '../components/StatsSection';
-import { ArrowRight } from 'lucide-react';
-import { Link } from 'react-router-dom';
-import DoctorsBySpecialtySection from '../components/DoctorsBySpecialtySection';
-import MediaMentionSection from '../components/MediaMentionSection';
+import React from "react";
+import Hero from "../components/Hero";
+import PartnersSection from "../components/PartnersSection";
+import DoctorsFavorite from "../components/DoctorsFavorite";
+import SpecialtySection from "../components/SpecialtySection";
+import DownloadAppSection from "../components/DownloadAppSection";
+import HomePosts from "../components/post/HomePosts";
+import StatsSection from "../components/StatsSection";
+import { ArrowRight } from "lucide-react";
+import { Link } from "react-router-dom";
+import DoctorsBySpecialtySection from "../components/DoctorsBySpecialtySection";
+import MediaMentionSection from "../components/MediaMentionSection";
+import ServiceSection from "../components/ServiceSection";
 
 const Home = () => {
   return (
     <>
       <Hero />
-<DoctorsBySpecialtySection />
-<DownloadAppSection />
+      <DoctorsBySpecialtySection />
+      <DownloadAppSection />
 
-         <DoctorsFavorite />
+      <DoctorsFavorite />
       <SpecialtySection />
-       <HomePosts />
+      <HomePosts />
       <MediaMentionSection />
+      <ServiceSection />
       <PartnersSection />
       <StatsSection />
-      
-
-
     </>
   );
 };
